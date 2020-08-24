f8c941f2fcf0612c6b3255803b70f8ccbb877185
keegers <42276368+keegers@users.noreply.github.com>
2020-08-24 10:52:43 -0700

    add searchPreview component and update some styling and comments

--- src/components/App/header/Header.jsx
+++ src/components/App/header/Header.jsx
@@ -7,8 +7,11 @@ const Header = ({ theme, useTheme }) => {
   console.log(theme);
   return (
     <div>
-      <Autocomplete list={['Hydrangea', 'Daisy', 'Cedar', 'Fern Spikey', 'Fern squishy']}/>
-
+      <Autocomplete list={[{ common_name: 'Hydrangea', scientific_name: 'latin latin 0', image_url: 'https://bs.floristic.org/image/o/7210dfc0c2a48bdde194bef37c7b7956974bc1b7' },
+        { common_name: 'Daisy', scientific_name: 'latin latin 1', image_url: 'https://bs.floristic.org/image/o/eb049be6b9186aed76ada3c8d3cd54d762842aa8' }, 
+        { common_name: 'Cedar', scientific_name: 'latin latin 2', image_url: 'https://bs.floristic.org/image/o/eb049be6b9186aed76ada3c8d3cd54d762842aa8' }, 
+        { common_name: 'Fern Spikey', scientific_name: 'latin latin 3', image_url: 'https://bs.floristic.org/image/o/eb049be6b9186aed76ada3c8d3cd54d762842aa8' }, 
+        { common_name:'Fern squishy', scientific_name: 'latin latin 4', image_url: 'https://bs.floristic.org/image/o/eb049be6b9186aed76ada3c8d3cd54d762842aa8' }]}/>
       <Toggle /> 
 
     </div>
