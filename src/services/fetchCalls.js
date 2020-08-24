58eebd350f7c96766c38d85b24006a49e6171eb0
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-08-24 16:14:45 -0700

    adds test for useList hook state, and it passes!

--- src/services/fetchCalls.js
+++ src/services/fetchCalls.js
@@ -1,7 +1,8 @@
 export const fetchPlantSearch = commonName => { 
-  return fetch(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants/search?token==${commonName}`)
+  return fetch(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/species/search?token=Un9xPcQrNglvNZM4l4Zi-UWswV1QUWdk9mJNya8MBxE=${commonName}`)
     .then(res => res.json())
     .then(plants => plants.data.map(plant => ({
+      id: plant.id,
       common_name: plant.common_name, 
       scientific_name: plant.scientific_name, 
       image: plant.image_url
