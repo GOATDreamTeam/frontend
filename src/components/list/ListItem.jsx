56f99d3d4e120b3a2d019ac4478893338b9ab21b
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-08-25 15:42:53 -0700

    added a linkto to our list item for connecting to detail page

--- src/components/list/ListItem.jsx
+++ src/components/list/ListItem.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const ListItem = ({ image_url, common_name, scientific_name }) => (
-  // <Link to="plant-details/:${id" >
-  <figure>
-    <img src={image_url} />
-    <figcaption>Common name: {common_name} - Scientific name: {scientific_name}</figcaption>
-  </figure>
-  // </Link>
+const ListItem = ({ image_url, common_name, scientific_name, id }) => (
+  <Link to={`/plant-details/:${id}`} >
+    <figure>
+      <img src={image_url} />
+      <figcaption>Common name: {common_name} - Scientific name: {scientific_name} - Id: {id}</figcaption>
+    </figure>
+  </Link>
 );
 
 ListItem.propTypes = {
   image_url: PropTypes.string.isRequired,
   common_name: PropTypes.string.isRequired,
-  scientific_name: PropTypes.string.isRequired
+  scientific_name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired
 };
 
 export default ListItem;
