6679b3a516724b003d247d6e911ab90323e613ea
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-08-25 20:34:26 -0700

    adding my description

--- src/components/aboutus/AboutUs.jsx
+++ src/components/aboutus/AboutUs.jsx
@@ -9,7 +9,7 @@ export default function AboutUs() {
       <div>
         {/* <img src={BriseidaProfile} alt="" /> */}
         <h3>Briseida </h3>
-        <p>Briseida is a developer that heavily relies on her mental health to carry on in this wild, wild world. She has been respecting quarantine for about 70 days now and deeply misses hugging, sharing meals, and listening to her friends laughing really hard.</p>
+        <p>Description:</p>
         <p>Github: https://github.com/bpagador</p>
         <p>Linkedin: https://www.linkedin.com/in/briseida-pagador/</p> <p>Portfolio: </p>
       </div>
@@ -17,7 +17,7 @@ export default function AboutUs() {
       <div>
         <img src="" alt="" />
         <h3>Breeann</h3>
-        <p>Description: Hey there! My name is Breeann and I was part of the team that created Fetch ART. I was really interested in this project as it combines development, art and design and loved to develop a new way to connect with lesser known creatives and their images. </p>
+        <p>Description: Hey there! My name is Breeann and I was part of the team that created FloraFile. I was really interested in this project was a combination of design-driven development and a passion for nature. I loved to develop a new way to bring tech and nature together in an informative way. </p>
         <p>Github: https://github.com/breeannb</p>
         <p>Linkedin: https://www.linkedin.com/in/breeannbolinsky/</p>
       </div>
