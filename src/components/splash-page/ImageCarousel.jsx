0fa72d70f1441e8c7807d664bbd424c4f67c33b6
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-08-26 14:42:49 -0700

    adding keyframe animation for rotating images

--- src/components/splash-page/ImageCarousel.jsx
+++ src/components/splash-page/ImageCarousel.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Image1 from '../../../images/flowers/image1.jpg';
+import Image2 from '../../../images/flowers/image2.jpg';
+import Image3 from '../../../images/flowers/image3.jpg';
+import Image4 from '../../../images/flowers/image4.jpg';
+import Image5 from '../../../images/flowers/image5.jpg';
+import styles from './ImageCarousel.css';
 
-export default function ImageCarousel() {
+function ImageCarousel() {
   return (
-    <div>
-      This is the image carousel.  
+    <div className={styles.Carousel}>
+      <img className={styles.f1} src={Image1} alt=""/> 
+      <img className={styles.f2} src={Image2} alt=""/> 
+      <img className={styles.f3} src={Image3} alt=""/> 
+      <img className={styles.f4} src={Image4} alt=""/> 
+      <img className={styles.f5} src={Image5} alt=""/> 
     </div>
+    
   );
 }
+
+export default ImageCarousel;
