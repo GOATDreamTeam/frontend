248bbaa529ff7a4400fc19105facc0e5b9fae987
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-08-25 20:21:46 -0700

    created rough outline of aboutus page

---
+++ src/components/aboutus/AboutUs.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+// import BriseidaProfile from '../../../images/profileimages/briseida.jpg';
+
+export default function AboutUs() {
+  return (
+    <div>
+      <h1>About Us Page</h1>
+
+      <div>
+        {/* <img src={BriseidaProfile} alt="" /> */}
+        <h3>Briseida </h3>
+        <p>Briseida is a developer that heavily relies on her mental health to carry on in this wild, wild world. She has been respecting quarantine for about 70 days now and deeply misses hugging, sharing meals, and listening to her friends laughing really hard.</p>
+        <p>Github: https://github.com/bpagador</p>
+        <p>Linkedin: https://www.linkedin.com/in/briseida-pagador/</p> <p>Portfolio: </p>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+        <h3>Breeann</h3>
+        <p>Description: Hey there! My name is Breeann and I was part of the team that created Fetch ART. I was really interested in this project as it combines development, art and design and loved to develop a new way to connect with lesser known creatives and their images. </p>
+        <p>Github: https://github.com/breeannb</p>
+        <p>Linkedin: https://www.linkedin.com/in/breeannbolinsky/</p>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+        <h3>Nikki</h3>
+        <p>Description:  </p>
+        <p>Github: https://github.com/nikki-kiga</p>
+        <p>Linkedin: https://www.linkedin.com/in/nikkikiga/</p>
+        <p>Portfolio: </p>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+        <h3>Langston</h3>
+        <p>Description:  </p>
+        <p>Github: https://github.com/orgs/GOATDreamTeam/people/langstonBS</p>
+        <p>Linkedin: https://www.linkedin.com/in/langston-beckwith-stanley/</p>
+        <p>Portfolio: </p>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+        <h3>Rachel</h3>
+        <p>Description:  </p>
+        <p>Github: https://github.com/PeepTheMoon</p>
+        <p>Linkedin: https://www.linkedin.com/in/rachelmdonahue/</p>
+        <p>Portfolio: </p>
+      </div>
+            
+    </div>
+  );
+}
+
+
